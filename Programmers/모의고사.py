1acf3f82cfa5fd40dd478e7be9d9dc4b01985c96
forma0-9097 <forma0.9097@gmail.com>
2025-03-29 09:19:30 +0900

    Create [완전탐색] 모의고사.py

---
+++ Programmers/모의고사.py
@@ -0,0 +1,15 @@
+def solution(answers):
+    scores = [0, 0, 0]
+
+    p1 = [1, 2, 3, 4, 5]
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    for i, ans in enumerate(answers):
+        if ans == p1[i % len(p1)]: scores[0] += 1
+        if ans == p2[i % len(p2)]: scores[1] += 1
+        if ans == p3[i % len(p3)]:scores[2] += 1
+    
+    max_score = max(scores)
+    
+    return [i+1 for i, score in enumerate(scores) if score == max_score]
